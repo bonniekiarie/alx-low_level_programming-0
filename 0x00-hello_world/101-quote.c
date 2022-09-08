a8365759dffbd58b7940d3ef0f6acabcc8b009b0
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-08 06:44:09 -0700

    Added betty foemating

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void){
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+/**
+ *main - takes no arguments
+ *
+ *Description:it prints an error message to the stderr
+ *
+ *Return: 0 showing an error
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" \
+			- Dora Korpar, 2015-10-19\n");
 	return (1);
 }
