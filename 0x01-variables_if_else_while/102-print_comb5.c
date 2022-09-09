898b05d2e247a34cd9b67f4e6d3eade7d8085bf8
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-09 05:30:49 -0700

    Refactored code for task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - takes no arguments
- * Description: prints unique numbers 0 -100
+ * Description: prints unique combination
+ * of two digit numbers
  * separated by commas
  * Return: 0 success program
  */
@@ -15,20 +16,20 @@ int main(void)
 		{
 			for (c = 48; c < 58; c++)
 			{
-				for (d= 48; d < 58; d++)
+				for (d = 48; d < 58; d++)
 				{
 					if ((a * 10 + b) < (c * 10 + d))
 					{
-						putchar(a);
-						putchar(b);
-						putchar(' ');
-						putchar(c);
-						putchar(d);
-						if (!(a == '9' && b == '8' && c == '9' && d == '9'))
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (!(a == '9' && b == '8' && c == '9' && d == '9'))
+					{
+					putchar(',');
+					putchar(' ');
+					}
 					}
 				}
 			}
