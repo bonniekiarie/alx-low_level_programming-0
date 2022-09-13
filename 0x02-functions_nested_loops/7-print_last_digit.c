ec01ffa80bea38030c15a1d034f847e20c95d077
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-13 09:15:49 -0700

    Added code for task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+/**
+ * print_last_digit - takes 0 arguments
+ * Description: extracts the last digit of a number
+ * Return: last digit of a number
+ */
+
+/**
+ * @n: integer argument
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
