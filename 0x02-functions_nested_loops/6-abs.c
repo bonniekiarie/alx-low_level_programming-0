57a86532f6ab642fd3d4504caefa5004d8225fdf
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-13 09:02:39 -0700

    added code for task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - takes one argument
+ * Description: calulates the absolute value
+ * Return: absolute value of the argument
+ */
+
+/**
+ * @n:integer argument
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+	return (n);
+}
