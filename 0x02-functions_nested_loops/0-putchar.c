442d9d36ffb65e0227c5c4cf5e2e3c71ee4f76d9
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-12 22:03:50 -0700

    Added read me file and code for task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - takes no arguments
+ * Description: prints _putchar
+ * Return: 0 on success of the program
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
