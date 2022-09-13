6ffa721971d51f8134f10a4ed062b852915576bc
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-13 09:25:44 -0700

    Added code for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - takes 2 arguments
+ * Description: adds up two numbers
+ * Return: sum of arguments
+ */
+/**
+ * @a: integer parameter
+ */
+/**
+ * @b: integer parameter
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
