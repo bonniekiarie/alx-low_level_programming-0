f59a2eefdeb8852bbe36297af6e622585abcb139
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-13 11:31:55 -0700

    added code for task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * print_to_98 - takes one argument
+ * Description: prints all natural numbers
+ * between parameter and 98
+ * Return: void
+ */
+
+/**
+ * @n: integer argument
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i++)
+		{
+			prinf("%d, ", i);
+		}
+	}
+	printf("%d", 98);
+}
