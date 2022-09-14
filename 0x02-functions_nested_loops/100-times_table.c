b18017de49c0e737f49ef3e922b4a770f088f7d7
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-13 23:59:06 -0700

    Updated c file for task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,17 +12,26 @@ void print_times_table(int n)
 {
 	int i, j, mul;
 
+	if (n > 0 && n <= 15)
+	{
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			mul = i * j;
-			if (mul >= 10)
+			if (mul >= 100)
 			{
+			_putchar('0' + mul / 100);
+			_putchar('0' + mul / 10);
+			}
+			else if (mul >= 10 && mul < 100)
+			{
+			_putchar(' ');
 			_putchar('0' + mul / 10);
 			}
 			else if (mul < 10 && j != 0)
 			{
+			_putchar(' ');
 			_putchar(' ');
 			}
 			_putchar('0' + mul % 10);
@@ -33,5 +42,6 @@ void print_times_table(int n)
 			}
 		}
 			_putchar('\n');
+	}
 	}
 }
