d2666c4ffccb012c845b654348153acb4e44e275
bonniekiarie <mugwira.kiarie@gmail.com>
2022-09-15 07:05:57 -0700

    Added code for task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+ * _isupper - takes a character as an argument
+ * Description: checks a cahracter is lower case or upper case
+ * Return: 1 if argurment is uppercase, 0 otherwise
+ */
+
+/**
+ * @c:single character parameter
+ */
+int _isupper(int c);
+{
+	if (c > 64 && c < 91)
+		return (1);
+	return (0);
+}
+
